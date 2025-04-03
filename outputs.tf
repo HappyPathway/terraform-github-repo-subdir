569dcf70da6b2ceda6eb19427f383c51a6852a78
Dave Arnold <dave@roknsound.com>
2025-04-02 18:14:45 -0700

    fix: correct module reference for HTTP clone URL output

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "ssh_clone_url" {
 }
 
 output "http_clone_url" {
-  value = module.repo.github_repos.http_clone_url
+  value = module.repo.github_repo.http_clone_url
 }
 
 output "repo_name" {
